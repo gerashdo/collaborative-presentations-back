516709476bc3231237176332bb5a5a4f40ec9221
gerashdo <gerardo.jimenezarguelles@gmail.com>
2024-09-20 11:36:42 -0600

    Add base configurations and test endpoint

---
+++ src/Server.ts
@@ -0,0 +1,49 @@
+import express, { Express } from "express"
+import cors from "cors"
+import dbConnection from "./db/config"
+import { ApiPaths } from "./interfaces/utils"
+import userRouter from "./routes/users"
+import { notFound } from "./controllers/404"
+
+
+export class Server {
+  private app: Express
+  private port: number
+  private paths: ApiPaths
+  private basePath: string
+
+  constructor() {
+    this.basePath = "/api/v1"
+    this.port = parseInt(process.env.PORT || "3000")
+    this.app = express()
+    this.paths = {
+      users: "/users",
+    }
+    this.connectDB()
+    this.middlwares()
+    this.routes()
+  }
+
+  connectDB() {
+    if (process.env.NODE_ENV !== 'test') {
+      dbConnection()
+    }
+  }
+
+  middlwares() {
+    this.app.use(cors())
+    this.app.use(express.static('public'))
+    this.app.use(express.json())
+  }
+
+  routes() {
+    this.app.use(`${this.basePath}${this.paths.users}`, userRouter)
+    this.app.get('*', notFound)
+  }
+
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log(`[server]: Server is running at port ${this.port}`)
+    })
+  }
+}
---
+++ src/interfaces/utils.ts
@@ -0,0 +1,4 @@
+
+export type ApiPaths = {
+  users: string
+}
